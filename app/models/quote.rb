f463048126e50a82eb947e91104893f9b405ae7b
alexclapou <alex.clapou@yahoo.com>
2025-04-04 15:11:54 +0300

    rating done

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,16 @@ class Quote < ApplicationRecord
   validates :content, length: { maximum: 255 }
 
   default_scope { order('created_at DESC') }
+
+  before_save :delete_rating_if_need
+
+  private
+
+  # remove rating if click old rating star
+  # we update rating only on index > click star
+  # safe to assume !changes[:content]
+  def delete_rating_if_need
+    self.rating = nil if self.rating == rating_was && !changes[:content]
+  end
+
 end
